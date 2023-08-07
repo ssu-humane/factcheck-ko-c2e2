c8a4df82181910bd3dfcff210ff5e8a356886a66
HUMANE Lab <80903024+ssu-humane@users.noreply.github.com>
2023-08-07 20:01:48 +0900

    Update Encoder.py

--- simcse/Encoder.py
+++ simcse/Encoder.py
@@ -2,11 +2,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Model(nn.Module):
-    def __init__(self, encoder, encoder_name):
+    def __init__(self, encoder):
         super(Model, self).__init__()
         self.dim = 768
         self.encoder = encoder
-        self.encoder_name = encoder_name 
         self.hidden = 100
         self.mlp_projection = nn.Sequential(nn.Linear(self.dim, self.hidden),
                                            nn.ReLU(),
